f16c8ee796ed749625dd366eb6786a146bdb7ed0
youssefsaoubou <youssef.saoubou@gmail.com>
2013-11-08 10:17:04 +0000

    index changé

--- themes/ourtheme/index.php
+++ themes/ourtheme/index.php
@@ -7,7 +7,7 @@
  * It is used to display a page when nothing more specific matches a query.
  * E.g., it puts together the home page when no home.php file exists.
  * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
+ * test youssef
  * @package Fundify
  * @since Fundify 1.0
  */
